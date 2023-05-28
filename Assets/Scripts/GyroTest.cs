a41d9a83b01effa7823dba37297b51accf9b7932
Jun Choi <52606638+bltaatld@users.noreply.github.com>
2023-05-28 16:06:10 +0900

    자이로 & 물 구현

--- Assets/Scripts/GyroTest.cs
+++ Assets/Scripts/GyroTest.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scripts : MonoBehaviour
+public class GyroTest : MonoBehaviour
 {
     private Gyroscope gyro;
+    private Quaternion initialRotation;
 
     void Start()
     {
         // 자이로 센서를 초기화합니다.
         gyro = Input.gyro;
         gyro.enabled = true;
+
+        // 초기 회전값을 저장합니다.
+        initialRotation = gyro.attitude;
     }
 
     void Update()
@@ -18,10 +22,20 @@ public class Scripts : MonoBehaviour
         // 자이로 센서의 회전 값을 가져옵니다.
         Quaternion rotation = gyro.attitude;
 
+        // 초기 회전값을 기준으로 상대적인 회전값을 계산합니다.
+        Quaternion relativeRotation = Quaternion.Inverse(initialRotation) * rotation;
+
         // Z축 회전 각도를 추출합니다.
-        float zAngle = rotation.eulerAngles.z;
+        float zAngle = relativeRotation.eulerAngles.z;
 
         // 오브젝트의 회전을 설정합니다.
         transform.rotation = Quaternion.Euler(0f, 0f, zAngle);
+    }
+
+    // 회전값을 초기화하는 함수
+    public void ResetRotation()
+    {
+        // 초기 회전값을 다시 설정합니다.
+        initialRotation = gyro.attitude;
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,71 +7,83 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Transform weed;
-    public float speed;
-    public float distanceFromWall = 0.1f;
-    public LayerMask wallLayer;
-    public float moveDuration = 3f;
-    public float moveTimer = 0f;
+	public Transform weed;
+	public float speed;
+	public float distanceFromWall = 0.1f;
+	public LayerMask wallLayer;
+	public float moveDuration = 3f;
+	public float moveTimer = 0f;
+	public SpriteRenderer curserRender;
 
-    private bool isMoving = false;
-    private Vector2 targetPosition;
-    private Vector2 startPosition;
+	public bool isMoving = false;
+	private Vector2 targetPosition;
+	private Vector2 startPosition;
 
-    void Update()
+	void Update()
+	{
+		transform.Rotate(Vector3.back * speed * Time.deltaTime);
+		if (Input.GetMouseButtonDown(0)&&!isMoving)
+		{
+			isMoving = true;
+			moveTimer = 0f;
+			startPosition = transform.position;
+
+			// 플레이어 회전 값
+			float playerAngle = transform.eulerAngles.z;
+
+			// 각도를 라디안으로
+			float angleInRadians = playerAngle * Mathf.Deg2Rad;
+
+			// 플레이어의 회전을 적용하여 Raycast 방향을 설정합니다.
+			Vector2 playerDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+
+			// Raycast를 수행하여 벽과의 충돌을 감지합니다.
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection, Mathf.Infinity, wallLayer);
+
+			// Raycast가 벽에 맞았을 때의 처리를 수행합니다.
+			if (hit.collider != null)
+			{
+				Vector2 hitPoint = hit.point;
+				targetPosition = hitPoint - playerDirection * distanceFromWall;
+			}
+
+			Quaternion currentRotation = transform.rotation;
+
+			// 현재 rotation의 z 값을 180도 회전합니다.
+			currentRotation *= Quaternion.Euler(0f, 0f, 180f);
+
+			// 변경된 rotation 값을 적용합니다.
+			transform.rotation = currentRotation;
+		}
+
+		if (isMoving)
+		{
+			moveTimer += Time.deltaTime;
+
+			if (moveTimer <= moveDuration)
+			{
+				float t = moveTimer / moveDuration;
+				transform.position = Vector2.Lerp(startPosition, targetPosition, t);
+                curserRender.gameObject.SetActive(false);
+            }
+			else
+			{
+				isMoving = false;
+                curserRender.gameObject.SetActive(true);
+            }
+		}
+	}
+
+    /*public void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.Rotate(Vector3.back * speed * Time.deltaTime);
-        if (Input.GetMouseButtonDown(0))
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            isMoving = true;
-            moveTimer = 0f;
-            startPosition = transform.position;
-
-            // 플레이어 회전 값
-            float playerAngle = transform.eulerAngles.z;
-
-            // 각도를 라디안으로
-            float angleInRadians = playerAngle * Mathf.Deg2Rad;
-
-            // 플레이어의 회전을 적용하여 Raycast 방향을 설정합니다.
-            Vector2 playerDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
-
-            // Raycast를 수행하여 벽과의 충돌을 감지합니다.
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection, Mathf.Infinity, wallLayer);
-
-            // Raycast가 벽에 맞았을 때의 처리를 수행합니다.
-            if (hit.collider != null)
-            {
-                Vector2 hitPoint = hit.point;
-                targetPosition = hitPoint - playerDirection * distanceFromWall;
-            }
-
-            Quaternion currentRotation = transform.rotation;
-
-            // 현재 rotation의 z 값을 180도 회전합니다.
-            currentRotation *= Quaternion.Euler(0f, 0f, 180f);
-
-            // 변경된 rotation 값을 적용합니다.
-            transform.rotation = currentRotation;
+            Debug.Log("Hit");
+            speed *= -1f;
         }
+    }*/
 
-        if (isMoving)
-        {
-            moveTimer += Time.deltaTime;
-
-            if (moveTimer <= moveDuration)
-            {
-                float t = moveTimer / moveDuration;
-                transform.position = Vector2.Lerp(startPosition, targetPosition, t);
-            }
-            else
-            {
-                isMoving = false;
-            }
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ground"))
         {
